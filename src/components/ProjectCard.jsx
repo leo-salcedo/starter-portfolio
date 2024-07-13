8b15542771fc9f434260634e80d198b16a167647
Leo Salcedo <leosalcedo364@gmail.com>
2024-07-12 22:18:35 -0500

    Fixed project card file

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,11 +4,11 @@ const ProjectCard = ({ imgUrl, title, description }) => {
   return (
     <div>
       <div
-        className="h-52 md:h-72"
+        className="h-52 md:h-72 rounded-t-xl"
         style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
       ></div>
-      <div className="text-white">
-        <h5>{title}</h5>
+      <div className="text-white rounded-b-xl bg-[#181818]py-6 px-4">
+        <h5 className="text-xl font-semibold mb-2">{title}</h5>
         <p>{description}</p>
       </div>
     </div>
--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,19 +1,46 @@
-import React from 'react'
-import ProjectCard from './ProjectCard'
+import React from "react";
+import ProjectCard from "./ProjectCard";
 
 const projectsData = [
-    {
-        id: 1,
-        title: "React Portfolio Website",
-        description: "Project 1 description",
-        image: 
-    }
-]
+  {
+    id: 1,
+    title: "React Portfolio Website",
+    description: "Project 1 description",
+    image: "/images/1.png",
+    tag: ["All", "Web"],
+  },
+  {
+    id: 2,
+    title: "Java Dig Dug Clone",
+    description: "Project 2 description",
+    image: "/images/2.png",
+    tag: ["All", "Games"],
+  },
+  {
+    id: 3,
+    title: "Java Multiplayer Battleship",
+    description: "Project 3 description",
+    image: "/images/3.png",
+    tag: ["All", "Games"],
+  },
+];
 
 const ProjectsSection = () => {
   return (
-    <><h2>My Projects</h2><div></div></>
-  )
-}
+    <>
+      <h2>My Projects</h2>
+      <div>
+        {projectsData.map((project) => (
+          <ProjectCard
+            key={project.id}
+            title={project.title}
+            description={project.description}
+            imgUrl={project.image}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default ProjectsSection
+export default ProjectsSection;
